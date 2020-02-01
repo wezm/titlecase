14af2ba52a12e598e13a11aefc97595d20160f19
Wesley Moore <wes@wezm.net>
2020-02-01 12:30:14 +1100

    Add --help and --version options
    Closes #2

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
 extern crate titlecase;
 
+use std::env;
 use std::io::{self, BufRead};
 use titlecase::titlecase;
 
 fn main() {
-    let stdin = io::stdin();
+    match env::args().nth(1).as_ref().map(String::as_str) {
+        Some("-h") | Some("--help") => return help(),
+        Some("-v") | Some("--version") => return version(),
+        Some(option) => return eprintln!("unknown option {}", option),
+        _ => (),
+    }
 
+    let stdin = io::stdin();
     for line in stdin.lock().lines() {
         match line {
             Ok(line) => println!("{}", titlecase(&line)),
@@ -14,4 +21,22 @@ fn main() {
             }
         }
     }
+}
+
+fn help() {
+    println!(
+        "\
+Usage: titlecase [OPTIONS]
+
+titlecase reads lines from stdin and applies title casing rules to each line,
+outputting the result on stdout.
+
+Optional arguments:
+  -h, --help     print help message
+  -v, --version  print the version"
+    );
+}
+
+fn version() {
+    println!("titlecase {}", env!("CARGO_PKG_VERSION"));
 }
